65d2c3fcb913fceba123351faa463d51dbd8fb49
MOJO196 <movhet@gmail.com>
2021-11-18 21:22:56 +0100

    Pong

--- UnityProjects/Assets/Scripts/Pong/PongBallMovement.cs
+++ UnityProjects/Assets/Scripts/Pong/PongBallMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Net;
+using System.Runtime.CompilerServices;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,7 +19,16 @@ public class PongBallMovement : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other)
-    {
-        rb.velocity = -rb.velocity;
+    {                
+        switch(other.name)
+        {
+            case "PongPlayer1":
+                rb.velocity = new Vector2(-GameObject.Find("PongPlayer1").transform.position.x, -GameObject.Find("PongPlayer1").transform.position.y);
+                Debug.Log(GameObject.Find("PongPlayer1").transform.position.x+", "+GameObject.Find("PongPlayer1").transform.position.y);
+            break;
+            case "PongPlayer2":
+                rb.velocity = new Vector2(-GameObject.Find("PongPlayer2").transform.position.x, -GameObject.Find("PongPlayer1").transform.position.y);
+            break;    
+        }
     }
 }
