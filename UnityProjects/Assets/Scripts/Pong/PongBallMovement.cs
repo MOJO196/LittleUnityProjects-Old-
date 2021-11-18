699e4b62490f6859f5841860344c5a568f8f51c6
MOJO196 <movhet@gmail.com>
2021-11-18 15:42:15 +0100

    Pong

---
+++ UnityProjects/Assets/Scripts/Pong/PongBallMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongBallMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        rb.velocity = -rb.velocity;
+    }
+}
---
+++ UnityProjects/Assets/Scripts/Pong/PongPlayerControll.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PongPlayerControll : MonoBehaviour
+{   
+    public float speed;
+    private Rigidbody2D rb;
+    private float pressTime;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Up(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed);
+        }
+        else if (context.canceled)
+        {
+            rb.velocity = new Vector2(0,0);
+        }
+        //rb.position = new Vector2(rb.position.x, rb.position.y+speed);
+    }
+
+    public void Down(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -speed);
+        }
+        else if (context.canceled)
+        {
+            rb.velocity = new Vector2(0,0);
+        }
+        //rb.position = new Vector2(rb.position.x, rb.position.y-speed);
+    }
+}
