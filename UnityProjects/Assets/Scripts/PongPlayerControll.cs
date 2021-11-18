fe0b9bdc5ea3ab20347b300e9ddc17772e5d59ed
MOJO196 <movhet@gmail.com>
2021-11-18 11:51:07 +0100

    Pong

---
+++ UnityProjects/Assets/Scripts/PongPlayerControll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PongPlayerControll : MonoBehaviour
+{
+    private float speed;
+    private int playerID;
+    private string playerInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playerID == 0)
+        {
+            playerInput = "PongPlayer1";
+        } 
+        else if (playerID == 1) 
+        {
+            playerInput = "PongPlayer2";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
