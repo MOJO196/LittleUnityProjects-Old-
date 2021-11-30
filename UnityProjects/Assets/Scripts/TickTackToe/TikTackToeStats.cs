218f5019fd99b84e170cd5ad741930c47e9692d9
MOJO196 <movhet@gmail.com>
2021-11-30 10:15:12 +0100

    TickTackToe

---
+++ UnityProjects/Assets/Scripts/TickTackToe/TikTackToeStats.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+public static class TikTackToeStats
+{
+    public static bool player1ToMove = true;
+    public static bool gameRunning = true;
+    public static bool draw;
+    public static bool player1Won;
+    public static bool player2Won;
+}
